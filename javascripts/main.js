5f8b980a8c1d6d8595ecfc1bf06e805c7e5370e4
Fedor Kurilov <fkuril@mail.ru>
2016-03-11 19:07:06 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
 console.log('This would be the main JS file.');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/my-app/sw.js', {
+    scope: '/my-app/'
+  }).then(function(reg) {
+    console.log('Yey!', reg);
+  }).catch(function(err) {
+    console.log('Boo!', err);
+  });
+}
